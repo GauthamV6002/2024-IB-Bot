c7ad2e3ee97d3a7f92dc07d66912502d20a62e4a
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-07-05 17:29:47 -0600

    test change

--- commands/test.js
+++ commands/test.js
@@ -7,40 +7,6 @@ module.exports = {
 		.setDescription("Testing Function for Bot Developer(s)"),
 		
 	async execute(interaction, client) {
-		
-		const embed = new MessageEmbed()
-			.setColor('GOLD')
-			.setTitle('TITLE')
-			.setDescription('Description!')
-			.addFields(
-				{ name: 'Armout', value: '+3HP'},
-				{ name: 'cannon', value: '+2Attack'},
-			);		
-		
-		const row = new MessageActionRow().addComponents(
-			new MessageButton()
-				.setCustomId("primary")
-				.setLabel("Primary")
-				.setStyle("SUCCESS")
-		);
-
-		const collector = interaction.channel.createMessageComponentCollector({
-			filter: (i) => i.customId === "primary",
-			time: 15000,
-		});
-
-		collector.on("collect", async (i) => {
-			if (i.customId === "primary"){
-				i.update({ components: [row] })
-			}
-		});
-
-		await interaction.reply({
-			content: "Pong!",
-			embeds: [embed],
-			components: [row],
-		});
-
-		// interaction.reply({ content: "Test Completed.", ephemeral: true })
+		interaction.reply({ content: "Test Completed.", ephemeral: true })
 	},
 };
