b7e0f9e8e712f456490431d5d087d058e9d42f1c
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-07-05 20:05:36 -0600

    added snowhelp cmd

---
+++ commands/snowhelp.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('snowhelp')
+        .setDescription('Official Help Command for the snowfight.'),
+    async execute(interaction, client) {    
+        const embed = new MessageEmbed()
+			.setColor("#b3fcff")
+			.setTitle("Snow Help")
+			.setDescription(`A list of commands for the snowball fight! All commands use the *slash* feature on discord.`)
+			.addFields(
+				{ name: "/collect", value: "Collect a snowball." },
+				{ name: "/throw [@target]", value: "Throw a a snowball at someone!" },
+				{ name: "/snowstats", value: "Get your statitics for the snowball fight. Shows attack, max health, snow points, and more. Wondering why its so complicated? We're ib." },
+				{ name: "/leaderboard", value: "Shows the snowball fight leaderboard, organized by snowpoints." },
+				{ name: "/store", value: "A store to buy items for the snowball fight." },
+				{ name: "/snowhelp", value: "Shows this command." }
+			);
+        interaction.reply({ embeds: [embed] });
+    },
+}
