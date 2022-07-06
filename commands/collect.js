f13beb6ea350e4ff29ebf1d09c5d2d53cfbabdd7
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-07-06 15:36:27 -0600

    offline no collect ha

--- commands/collect.js
+++ commands/collect.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { getOrNewUser, getOrNewSnowballUserCache } = require("../db-commands.js");
+const { guildId } = require("../config.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,6 +8,19 @@ module.exports = {
         .setDescription('Collect & Make a Snowball'),
     async execute(interaction, client) {
         if(client.checkKO(client, interaction)) return;
+
+        const guild = client.guilds.cache.get(guildId);
+		const userMember = guild.members.cache.get((interaction.user.id).toString());
+
+		try {	
+			if(userMember.presence.status === "offline"){
+				interaction.reply({ content: "You can't collect when offline/invisible!", ephemeral: true });
+				return;
+			}
+		} catch (e) {
+			interaction.reply({ content: "You can't collect when offline/invisible!", ephemeral: true });
+			return;
+		}
 
         const user = await getOrNewUser(interaction.user.id, interaction.user.tag);
         if (user.snowCurrentSnowballs >= client.maxSnowballs){
