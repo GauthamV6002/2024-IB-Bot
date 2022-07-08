6ceeef87ad732cab619961428c7bd7df8081ce61
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-07-08 16:10:30 -0600

    ERRORS

--- commands/store.js
+++ commands/store.js
@@ -74,9 +74,14 @@ module.exports = {
             collector.on("collect", async (i) => {
                 user = await getOrNewUser(i.user.id)
                 if (user.snowPoints - item.cost < 0) {
-                    i.update({ content: `You're too poor. You only have ${user.snowPoints} :snowflake:.`, ephemeral: true});
+                    i.update({ content: `You're too poor. You only have ${user.snowPoints} :snowflake:.`, ephemeral: true, embeds: [], components: []});
 				} else if (user[item.stat] + item.adder > Number(item.cap) && item.cap) {
-					i.update(`❌ You can't buy anymore of this item! The cap for this item is ${item.cap}.`);
+					i.update({
+						content: `❌ You can't buy anymore of this item! The cap for this item is ${item.cap}.`,
+						ephemeral: true,
+						embeds: [],
+						components: []
+					});
 				} else {
 					user.snowPoints -= item.cost;
 					user[item.stat] += item.adder;
@@ -89,9 +94,12 @@ module.exports = {
 						}, item.timer);
 					}
 
-					i.update(
-						`${item.emoji} ${item.name} was successfully bought by ${interaction.user}!`
-					);
+					i.update({
+						content: `${item.emoji} ${item.name} was successfully bought by ${interaction.user}!`,
+						ephemeral: true,
+						embeds: [],
+						components: [],
+					});
 				}
             });
 
