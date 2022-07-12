fe9bda0fe6b10044994432cfade2dbe6169b6af2
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-07-11 23:28:57 -0600

    added str option for channel select (btn)

--- commands/admin.js
+++ commands/admin.js
@@ -6,14 +6,19 @@ const BTN_CHANNEL = "994703495846449162";
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('admin')
-        .setDescription('Admin Command'),
+        .setDescription('Admin Command')
+        .addStringOption(option =>
+            option.setName('args')
+                .setDescription('Arguments')
+                .setRequired(true)),
     async execute(interaction, client) {
         if (interaction.user.id !== "901273742901133393"){
-			interaction.reply({ content: "This command is only meant for bot developer(s) & Admins.", ephemeral: true });
+			interaction.reply({ content: "This command is only meant for bot developer(s) & admins.", ephemeral: true });
 			return;
 		}
-
-        createRoleButton(client, BTN_CHANNEL);
+        
+        const args = interaction.options.getString('args');
+        createRoleButton(client, args);
         interaction.reply({content: "Test completed.", ephemeral: true })
     },
 }
